8d23a7bedd808f4d237903cb5cdbdf21e3a3f9ae
kanarabhisek20-png <kanarabhisk20@gmail.com>
2025-12-15 13:05:39 +0530

    dom class

---
+++ day8/first.js
@@ -0,0 +1,17 @@
+console.log("Hello welcome to GIET Ghangapatna");
+// variables
+// rules of variable declaration
+// rule:1
+    //     only DOMStringList, characters,_,$ we can use
+// rule 2:
+    //     variables are case sensitive
+// rule 3:
+    //     The first letter should not be digit
+//rule 4:
+    //     Reserve keywords are not possible for declare a variable
+
+//example:
+    a=20;
+    console.log(a);
+    A=40;
+    console.log(A);
