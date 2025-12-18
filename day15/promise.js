84d2f443ab33253f9da9cffe9bd09a266a93b638
kanarabhisek20-png <kanarabhisk20@gmail.com>
2025-12-18 12:55:10 +0530

    promise class

---
+++ day15/promise.js
@@ -0,0 +1,63 @@
+console.log("today we are going to learn js promise");
+
+document.addEventListener("keydown", (e) => {
+    console.log(e.key);
+    console.log(e.code);
+    console.log(e.keyCode);
+    console.log(e.ctrlKey);
+    console.log(e.altKey);
+    console.log(e.shiftKey);
+    // console.log("key down")
+})
+
+// // document.addEventListener("keyup", () => {
+// //     console.log("key up");
+// // })
+
+// document.addEventListener("keypress", () => {
+//     console.log("key press");
+// })
+
+let inputfield=document.querySelector("#input-tag");  //for targeting
+inputfield.addEventListener("input",(e)=>{
+    console.log(e)
+    console.log("input event");
+})
+
+// inputfield.addEventListener("change",()=>{
+//     console.log("change event");
+// })
+
+// inputfield.addEventListener("focus",()=>{
+//     console.log("focus event");
+// })
+
+// inputfield.addEventListener("blur",()=>{
+//     console.log("blur event");
+// })
+
+let form=document.querySelector("form");
+    form.addEventListener("submit",(evl) =>{
+        console.log(evl)
+        evl.preventDefault()
+    console.log("submit")
+})
+
+// //window
+// // window.addEventListener("load",()=>{
+// //     console.log("load")
+// // })
+
+window.addEventListener("DOMconentLoaded",(e)=>{
+    console.log("dom load")
+    console.log(e);
+})
+
+// window.addEventListener("resize",()=>{
+//     console.log("resize")
+// })
+
+// window.addEventListener("scroll",()=>{
+//     console.log("scroll")
+// })
+
